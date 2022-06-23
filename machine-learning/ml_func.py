f87c7aca3b9291375ab5515e3eb1d7bd797950e9
vuillaut <thomas.vuillaume@gmail.com>
2022-06-24 00:32:28 +0200

    rm sns

--- machine-learning/ml_func.py
+++ machine-learning/ml_func.py
@@ -31,7 +31,6 @@ def download_file(url, filename, force=False):
                 sys.stdout.flush()
 
 def set_plot_style():
-    sns.reset_orig()
     plt.rcParams["figure.figsize"] = (9.23, 9.23 / 3 * 2)
     plt.rcParams["figure.dpi"] = 100
     plt.rcParams["figure.max_open_warning"] = 50
