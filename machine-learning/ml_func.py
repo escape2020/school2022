6b791b78bba5266ac62895b55ad4439f936a6d3c
vuillaut <thomas.vuillaume@gmail.com>
2022-06-23 23:30:00 +0200

    function to download  data

--- machine-learning/ml_func.py
+++ machine-learning/ml_func.py
@@ -12,6 +12,9 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 from matplotlib.colors import ListedColormap, to_hex
+from pathlib import Path
+import requests
+import sys
 
 from ctapipe.instrument import SubarrayDescription
 
@@ -26,6 +29,25 @@ cmap = ListedColormap(colors)
 gamma_filename = get_dataset_path('gamma_diffuse_dl2_train_small.dl2.h5')
 proton_filename = get_dataset_path('proton_dl2_train_small.dl2.h5')
 
+# a function to download a file from a url
+def download_file(url, filename, force=False):
+    if Path(filename).exists() and not force:
+        print(f"File {filename} exists already")
+        return None
+    with open(filename, "wb") as f:
+        response = requests.get(url, stream=True)
+        total_length = response.headers.get('content-length')   # total length of the file
+        if total_length is None: # no content length header - just download the file
+            f.write(response.content)
+        else:
+            dl = 0  # downloaded size
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=1024):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+                sys.stdout.flush()
 
 def set_plot_style():
     sns.reset_orig()
